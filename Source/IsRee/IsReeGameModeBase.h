ae845f0ece68b7d5c6e51a25fa687bd0b325a282
Hyunmo Jung <hm9397@naver.com>
2022-11-30 22:53:04 +0900

    Test1

---
+++ Source/IsRee/IsReeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IsReeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ISREE_API AIsReeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
